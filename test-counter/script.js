327c4f2529a340c0c9c5b3b1609015f89bfd6a57
innoviflow <142358277+innoviflow@users.noreply.github.com>
2024-02-02 14:59:59 -0500

    Added Score

--- test-counter/script.js
+++ test-counter/script.js
@@ -3,23 +3,37 @@ const incorrectButton = document.getElementById('incorrectButton');
 const resetButton = document.getElementById('resetButton');
 const correctCountElement = document.getElementById('correctCount');
 const incorrectCountElement = document.getElementById('incorrectCount');
+const scoreElement = document.getElementById('score');
 
 let correctCount = 0;
 let incorrectCount = 0;
+let score=0;
 
 correctButton.addEventListener('click', () => {
   correctCount++;
   correctCountElement.textContent = correctCount;
+  evaluateScore();
 });
 
 incorrectButton.addEventListener('click', () => {
   incorrectCount++;
   incorrectCountElement.textContent = incorrectCount;
+  evaluateScore();
 });
 
 resetButton.addEventListener('click', () => {
   correctCount = 0;
   incorrectCount = 0;
+  evaluateScore();
   correctCountElement.textContent = correctCount;
   incorrectCountElement.textContent = incorrectCount;
 });
+
+function evaluateScore () {
+  if ((correctCount + incorrectCount)>0)
+  score = Math.min(0,100*correctCount/(correctCount + incorrectCount));
+else 
+  score = 0;
+
+  scoreElement.textContent = score;
+}
