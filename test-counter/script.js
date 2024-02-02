8e263462bb48569d949e19397c87edcac0229a27
innoviflow <142358277+innoviflow@users.noreply.github.com>
2024-02-02 15:17:02 -0500

    Score formula update

--- test-counter/script.js
+++ test-counter/script.js
@@ -31,7 +31,7 @@ resetButton.addEventListener('click', () => {
 
 function evaluateScore () {
   if ((correctCount + incorrectCount)>0)
-  score = Math.max(0,100*correctCount/(correctCount + incorrectCount));
+  score = Math.max(0,Math.floor(100*correctCount/(correctCount + incorrectCount)));
 else 
   score = 0;
 
